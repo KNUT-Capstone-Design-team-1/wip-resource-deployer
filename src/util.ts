e711dcff57e5215a484e80db5b1b080431e1d1a3
YongSiHun <tlgns1537@naver.com>
2024-11-05 00:03:20 +0900

    first commit

---
+++ src/util.ts
@@ -0,0 +1,5 @@
+export function convertStringToInt8Array(int8ArrayFormatString: string): Int8Array {
+  const res = int8ArrayFormatString.match(/-?\d+/g)?.map(Number);
+
+  return new Int8Array(res ?? []);
+}
