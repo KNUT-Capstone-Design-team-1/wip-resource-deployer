efe66a821ad574c7c35723099515b706ab48d7cb
YongSiHun <tlgns1537@naver.com>
2024-11-05 23:27:51 +0900

    #1 리소스 레지스트리 클래스 가구현

--- src/resource_registry/cloud_flare_r2_client.ts
+++ src/resource_registry/cloud_flare_r2_client.ts
@@ -1,5 +1,38 @@
+import {
+  S3Client,
+} from "@aws-sdk/client-s3";
+
 export class CloudFlareR2Client {
-  public async authenticate() {}
+  private readonly apiURL: string;
+  private readonly bucket: string;
+  private readonly accessKeyID: string;
+  private readonly secretAccessKey: string;
+  private readonly resourcePath: string;
+
+  constructor() {
+    const {
+      CLOUD_FLARE_RESOURCE_DOWNLOAD_URL,
+      CLOUD_FLARE_RESOURCE_BUCKET,
+      CLOUD_FLARE_ACCESS_KEY_ID,
+      CLOUD_FLARE_SECRET_ACCESS_KEY,
+    } = process.env;
+
+    this.apiURL = CLOUD_FLARE_RESOURCE_DOWNLOAD_URL as string;
+    this.bucket = CLOUD_FLARE_RESOURCE_BUCKET as string;
+    this.accessKeyID = CLOUD_FLARE_ACCESS_KEY_ID as string;
+    this.secretAccessKey = CLOUD_FLARE_SECRET_ACCESS_KEY as string;
+  }
 
   public async upload() {}
+
+  private getS3Client() {
+    return new S3Client({
+      endpoint: this.apiURL,
+      region: "auto",
+      credentials: {
+        accessKeyId: this.accessKeyID,
+        secretAccessKey: this.secretAccessKey,
+      },
+    });
+  }
 }
