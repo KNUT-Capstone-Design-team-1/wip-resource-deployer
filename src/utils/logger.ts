028ec7edb5dbac26777202dc3607bb13218d28e5
YongSiHun <tlgns1537@naver.com>
2024-11-20 23:58:56 +0900

    log: 로그 위치 변경
    - 로그 파일 .gitignore에 추가

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,19 +1,26 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from "winston";
 
-const PATH = 'logs';
+const logDirectoryName = "log";
 
 const { combine, timestamp, printf, splat } = format;
 
 const logger = createLogger({
   format: combine(
     splat(),
-    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    printf(({ timestamp: time, level, message }) => `${time} [${level}] ${message}`)
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    printf(
+      ({ timestamp: time, level, message }) => `${time} [${level}] ${message}`
+    )
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: `${PATH}/wip-resource-deployer.log` }),
-    new transports.File({ level: 'error', filename: `${PATH}/error.log` }),
+    new transports.File({
+      filename: `${logDirectoryName}/wip-resource-deployer.log`,
+    }),
+    new transports.File({
+      level: "error",
+      filename: `${logDirectoryName}/error.log`,
+    }),
   ],
 });
 
