6d19c3d1cf9fe2f94225a823e302ff2c6ed041e6
YongSiHun <tlgns1537@naver.com>
2024-11-24 03:14:22 +0900

    #1 feat: 업데이트 리소스 파일 생성 로직 구현

--- src/services/resource_update.ts
+++ src/services/resource_update.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import {
   DrugRecognitionModel,
   FinishedMedicinePermissionDetailModel,
@@ -5,19 +6,53 @@ import {
 } from "../models";
 import {
   CURRENT_INITIAL_REALM_FILE_NAME,
+  getObjectArrayDiff,
   logger,
   NEW_INITIAL_REALM_FILE_NAME,
   UPDATE_REALM_FILE_NAME,
 } from "../utils";
+import {
+  IDrugRecognition,
+  IFinishedMedicinePermissionDetail,
+  TLoadedResource,
+} from "../@types";
+
+function createUpdateResourceData(
+  newRes: TLoadedResource,
+  currentRes: TLoadedResource
+): TLoadedResource {
+  const updateResourceData: TLoadedResource = {
+    drugRecognition: [],
+    finishedMedicinePermissionDetail: [],
+  };
+
+  updateResourceData.drugRecognition.push(
+    ...(getObjectArrayDiff(
+      newRes.drugRecognition,
+      "ITEM_SEQ",
+      currentRes.drugRecognition
+    ) as Array<IDrugRecognition>)
+  );
+
+  updateResourceData.finishedMedicinePermissionDetail.push(
+    ...(getObjectArrayDiff(
+      newRes.finishedMedicinePermissionDetail,
+      "ITEM_SEQ",
+      currentRes.finishedMedicinePermissionDetail
+    ) as Array<IFinishedMedicinePermissionDetail>)
+  );
+
+  return updateResourceData;
+}
 
 export async function createUpdateResourceFile() {
   logger.info("Read all data of current resource");
 
   await RealmDatabase.initInstance(CURRENT_INITIAL_REALM_FILE_NAME);
 
-  const currentResourceData = {
-    drugPermission: new DrugRecognitionModel().readAll(),
-    finisehdMedicinePermissionDetail:
+  const currentResourceData: TLoadedResource = {
+    drugRecognition: new DrugRecognitionModel().readAll(),
+    finishedMedicinePermissionDetail:
       new FinishedMedicinePermissionDetailModel().readAll(),
   };
 
@@ -25,11 +60,44 @@ export async function createUpdateResourceFile() {
 
   await RealmDatabase.initInstance(NEW_INITIAL_REALM_FILE_NAME);
 
-  const initialResourceData = {
-    drugPermission: new DrugRecognitionModel().readAll(),
+  const initialResourceData: TLoadedResource = {
+    drugRecognition: new DrugRecognitionModel().readAll(),
     finishedMedicinePermissionDetail:
       new FinishedMedicinePermissionDetailModel().readAll(),
   };
 
-  // await RealmDatabase.initInstance(UPDATE_REALM_FILE_NAME);
+  logger.info("Create update resource data");
+
+  const updateResourceData = createUpdateResourceData(
+    initialResourceData,
+    currentResourceData
+  );
+
+  const drugRecognitionUpdated = Boolean(
+    updateResourceData.drugRecognition.length
+  );
+  const finishedMedicinePermissionDetailUpdated = Boolean(
+    updateResourceData.finishedMedicinePermissionDetail.length
+  );
+
+  if (!drugRecognitionUpdated && !finishedMedicinePermissionDetailUpdated) {
+    logger.info("No updated data");
+    return;
+  }
+
+  logger.info("Update resource data is exist. create update resource file");
+
+  await RealmDatabase.initInstance(UPDATE_REALM_FILE_NAME);
+
+  if (drugRecognitionUpdated) {
+    new DrugRecognitionModel().upsertMany(updateResourceData.drugRecognition);
+  }
+
+  if (finishedMedicinePermissionDetailUpdated) {
+    new FinishedMedicinePermissionDetailModel().upsertMany(
+      updateResourceData.finishedMedicinePermissionDetail
+    );
+  }
+
+  logger.info("Create update resource file complete");
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,9 @@
 import path from "path";
+import _ from "lodash";
 import logger from "./logger";
 
 export const NEW_DATABASE_DIRECTORY_NAME = "./database_resource";
+
 export const NEW_INITIAL_REALM_FILE_NAME = path.join(
   `${NEW_DATABASE_DIRECTORY_NAME}/initial.realm`
 );
@@ -10,12 +12,14 @@ export const UPDATE_REALM_FILE_NAME = path.join(
 );
 
 export const CURRENT_DATABASE_DIRECTORY_NAME = "./current_database_resource";
+
 export const CURRENT_INITIAL_REALM_FILE_NAME = path.join(
   `${CURRENT_DATABASE_DIRECTORY_NAME}/initial.realm`
 );
 
 export const FINISHED_MEDICINE_PERMISSION_DETAIL =
   "FinishedMedicinePermissionDetail";
+
 export const DRUG_RECOGNITION = "DrugRecognition";
 
 export function convertStringToInt8Array(
@@ -24,6 +28,26 @@ export function convertStringToInt8Array(
   const res = int8ArrayFormatString.match(/-?\d+/g)?.map(Number);
 
   return new Int8Array(res ?? []);
+}
+
+export function getObjectArrayDiff(
+  criteria: Array<Record<string, any>>,
+  criteriaKey: string,
+  compare: Array<Record<string, any>>
+) {
+  const diff: Array<Record<string, any>> = [];
+
+  for (let i = 0; i < criteria.length; i += 1) {
+    const criteriaItem = criteria[i];
+  
+    const sameItem = compare.find((c) => c[criteriaKey] === criteriaItem[criteriaKey]);
+
+    if (!sameItem || !_.isEqual(criteriaItem, sameItem)) {
+      diff.push(criteriaItem);
+    }
+  }
+
+  return diff;
 }
 
 export { logger };
