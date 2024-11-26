d62fa7125da657acf2812cdad45f0279557cd4d1
YongSiHun <tlgns1537@naver.com>
2024-11-26 23:56:52 +0900

    #1 feat: Resource 업로드 기능 구현

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-import fs from "fs";
 import {
   createInitialResourceFile,
   CloudFlareDownloadService,
@@ -7,32 +6,23 @@ import {
   createUpdateResourceFile,
   CloudflareUploadService,
 } from "./services";
-import {
-  logger,
-  INITIAL_REALM_FILE_NAME,
-  UPDATE_REALM_FILE_NAME,
-} from "./utils";
+import { logger } from "./utils";
 
 async function main() {
   logger.info("------Create initial resource file------");
-  // await createInitialResourceFile();
-
-  logger.info("------Upload resource file------");
-  CloudFlareR2Client.initS3Client();
-  const resourceUploadService = new CloudflareUploadService();
-  await resourceUploadService.upload(INITIAL_REALM_FILE_NAME);
+  await createInitialResourceFile();
 
   logger.info("------Download current resource file------");
   const resourceDownloadService = new CloudFlareDownloadService();
-  // await resourceDownloadService.downloadAllResources();
+  await resourceDownloadService.downloadAllResources();
 
   logger.info("------Create update resource file------");
   await createUpdateResourceFile();
 
-  if (fs.existsSync(UPDATE_REALM_FILE_NAME)) {
-    logger.info("------Upload update resource file------");
-    await resourceUploadService.upload(UPDATE_REALM_FILE_NAME);
-  }
+  logger.info("------Upload resource file------");
+  CloudFlareR2Client.initS3Client();
+  const resourceUploadService = new CloudflareUploadService();
+  await resourceUploadService.uploadAllResources();
 
   logger.info("------End wip-resource-deployer------");
 }
