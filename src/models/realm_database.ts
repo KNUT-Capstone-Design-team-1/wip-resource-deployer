a1deb6969c765c6ce6b4f7e23746d862d9fc5732
YongSiHun <tlgns1537@naver.com>
2025-06-22 16:12:49 +0900

    DB: 스키마 버전 업그레이드
    - config 파일을 통해 조정하도록 개선

--- src/models/realm_database.ts
+++ src/models/realm_database.ts
@@ -1,13 +1,14 @@
 import Realm from "realm";
 import { PillDataSchema } from "../schemas";
 import { convertStringToInt8Array } from "../utils";
+import configJSON from "../../config.json";
 
 export class RealmDatabase {
   public static getInstance(realmFilepath: string): Realm {
     const config = {
       path: realmFilepath,
       schema: [PillDataSchema],
-      schemaVersion: 1,
+      schemaVersion: configJSON.schemaVersion,
       encryptionKey: convertStringToInt8Array(
         process.env.REALM_ENCRYPTION_KEY as string
       ),
