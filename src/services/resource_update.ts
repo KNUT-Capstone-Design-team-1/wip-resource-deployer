84355b53fb9ed13980764e32efebb2499fbce529
YongSiHun <tlgns1537@naver.com>
2025-07-05 17:51:50 +0900

    feat: 데이터베이스 리소스 업데이트 안되는 현상 수정
    #10

--- src/services/resource_update.ts
+++ src/services/resource_update.ts
@@ -40,7 +40,7 @@ export async function createUpdateResourceFile() {
   const currentResourceFilePath = path.join(DATABASE_DIRECTORY_NAME, "current");
   const currentResourceFiles = fs.readdirSync(currentResourceFilePath);
   const currentInitialResourceFileName = currentResourceFiles.find(
-    (v) => v?.split("_")?.[3] === (config.schemaMinorVersion)
+    (v) => v?.split("_")?.[3]?.split(".")?.[0] === config.schemaMinorVersion
   );
 
   if (!currentInitialResourceFileName) {
