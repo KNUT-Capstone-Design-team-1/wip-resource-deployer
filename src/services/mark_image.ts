2e96bb605aeaf2236928ace380c790940bca2051
YongSiHun <tlgns1537@naver.com>
2026-01-10 15:16:50 +0900

    bug: 마크 이미지 리소스 파일 생성 안되는 현상 수정

--- src/services/mark_image.ts
+++ src/services/mark_image.ts
@@ -1,13 +1,14 @@
 import path from "path";
 import fs from "fs";
 import { IMarkImageData } from "../types";
-import { MarkImageCrawler } from "../utils";
+import { logger, MarkImageCrawler } from "../utils";
 import config from "../../config.json";
 
 /**
- * 마크 이미지 리소스 파일 생성
+ * 마크 이미지 크롤링 요청
+ * @returns
  */
-export async function createMarkImageResource() {
+async function getImageDatas() {
   const { targetURL, totalPages, limit } = config.markImage;
 
   const imageDatas: Array<IMarkImageData> = [];
@@ -18,6 +19,13 @@ export async function createMarkImageResource() {
     imageDatas.push(...(await MarkImageCrawler.crawling(url)));
   }
 
+  return imageDatas;
+}
+
+/**
+ * 마크 이미지 리소스 파일 생성
+ */
+function createMarkImageResourceFile(imageDatas: Array<IMarkImageData>) {
   const filePath = path.resolve(__dirname, "../../mark_image_data.json");
 
   if (fs.existsSync(filePath)) {
@@ -25,4 +33,25 @@ export async function createMarkImageResource() {
   }
 
   fs.writeFileSync(filePath, JSON.stringify(imageDatas, null, 2));
+}
+
+/**
+ * 마크 이미지 리소스 파일 생성
+ */
+export async function createMarkImageResource() {
+  try {
+    logger.info("[MARK-IMAGE] Start crawling mark image data");
+
+    const imageDatas = await getImageDatas();
+
+    logger.info("[MARK-IMAGE] Complete crawling mark image data");
+
+    logger.info("[MARK-IMAGE] Start create mark image resource file");
+
+    createMarkImageResourceFile(imageDatas);
+
+    logger.info("[MARK-IMAGE] Complete create mark image resource file");
+  } catch (e) {
+    logger.error("[MARK-IMAGE] Failed to create mark image resource file");
+  }
 }
