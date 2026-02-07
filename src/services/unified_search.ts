7ef5aa76baf85bd7cb0d56545dd3b742257b8eb7
YongSiHun <tlgns1537@naver.com>
2026-02-07 22:38:53 +0900

    feat: 통합검색 데이터 생성 로직 준비

--- src/services/unified_search.ts
+++ src/services/unified_search.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { logger } from "src/utils";
 
 /**
  * 의약품 낱알식별 정보 데이터의 별도 문서 데이터를 의약품 안전나라에서 xml으로 받아온다
@@ -19,11 +20,11 @@ async function getDocData(itemSeq: string) {
       NB_DOC_DATA: NB.data,
     };
 
-    console.log("get data (%s)", itemSeq);
+    logger.info("get data (%s)", itemSeq);
 
     return nedrugData;
   } catch (e) {
-    console.log("Failed to get doc data. item_seq: %s. %s", e.stack || e);
+    logger.info("Failed to get doc data. item_seq: %s. %s", e.stack || e);
 
     return { EE_DOC_DATA: "", UD_DOC_DATA: "", NB_DOC_DATA: "" };
   }
