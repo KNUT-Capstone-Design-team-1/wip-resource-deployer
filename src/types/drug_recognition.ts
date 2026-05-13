8a4e31755a5508702ec6c4225d97d589ecb9793c
YongSiHun <tlgns1537@naver.com>
2026-05-13 21:37:46 +0900

    schema: 알약 데이터 컬럼 추가

--- src/types/drug_recognition.ts
+++ src/types/drug_recognition.ts
@@ -6,6 +6,7 @@ export interface IDrugRecognition {
   ITEM_NAME: string; // 품목명
   ENTP_SEQ: string; // 업소일련번호
   ENTP_NAME: string; // 업소명
+  CHART: string; // 성상
   ITEM_IMAGE: string; // 큰제품이미지
   PRINT_FRONT: string; // 표시앞
   PRINT_BACK: string; // 표시뒤
@@ -14,13 +15,26 @@ export interface IDrugRecognition {
   COLOR_CLASS2: string; // 색상뒤
   LINE_FRONT: string; // 분할선앞
   LINE_BACK: string; // 분할선뒤
+  LENGTH_LONG: string; // 크기장축
+  LENGTH_SHORT: string; // 크기단축
+  LENGTH_THICK: string; // 크기두께
   IMG_REGIST_TS: string; // 이미지생성일자(약학정보원)
+  CLASS_NO: string; // 분류번호
   CLASS_NAME: string; // 분류명
   ETC_OTC_CODE: string; // 전문일반구분
   ITEM_PERMIT_DATE: string; // 품목허가일자
+  FORM_CODE: string; // 제형코드명
+  DRUG_SHAPE_FRONT: string; // 표기내용앞
+  DRUG_SHAPE_BACK: string; // 표기내용뒤
+  MARK_IMAGE_FRONT: string; // 표기이미지앞
+  MARK_IMAGE_BACK: string; // 표기이미지뒤
   MARK_CODE_FRONT: string; // 표기코드앞
   MARK_CODE_BACK: string; // 표기코드뒤
-  FORM_CODE: string; // 제형코드명
+  CHANGE_DATE: string; // 변경일자
+  BUSINESS_LICENCE_NUMBER: string; // 사업자번호
+  ITEM_ENG_NAME: string; // 품목영문명
+  COVERAGE_ENG_NAME: string; // 보험코드
+  BAR_CODE: string; // 표준코드
 }
 
 export type TDrugRecognitionResource = Record<
@@ -33,6 +47,7 @@ export const DRUG_RECOGNITION_PROPERTY_MAP = {
   품목명: "ITEM_NAME",
   업소일련번호: "ENTP_SEQ",
   업소명: "ENTP_NAME",
+  성상: "CHART",
   큰제품이미지: "ITEM_IMAGE",
   표시앞: "PRINT_FRONT",
   표시뒤: "PRINT_BACK",
@@ -41,11 +56,24 @@ export const DRUG_RECOGNITION_PROPERTY_MAP = {
   색상뒤: "COLOR_CLASS2",
   분할선앞: "LINE_FRONT",
   분할선뒤: "LINE_BACK",
+  크기장축: "LENGTH_LONG",
+  크기단축: "LENGTH_SHORT",
+  크기두께: "LENGTH_THICK",
   "이미지생성일자(약학정보원)": "IMG_REGIST_TS",
+  분류번호: "CLASS_NO",
   분류명: "CLASS_NAME",
   전문일반구분: "ETC_OTC_CODE",
   품목허가일자: "ITEM_PERMIT_DATE",
+  제형코드명: "FORM_CODE",
+  표기내용앞: "DRUG_SHAPE_FRONT",
+  표기내용뒤: "DRUG_SHAPE_BACK",
+  표기이미지앞: "MARK_IMAGE_FRONT",
+  표기이미지뒤: "MARK_IMAGE_BACK",
   표기코드앞: "MARK_CODE_FRONT",
   표기코드뒤: "MARK_CODE_BACK",
-  제형코드명: "FORM_CODE",
+  변경일자: "CHANGE_DATE",
+  사업자번호: "BUSINESS_LICENCE_NUMBER",
+  품목영문명: "ITEM_ENG_NAME",
+  보험코드: "COVERAGE_ENG_NAME",
+  표준코드: "BAR_CODE",
 } as const;
--- src/types/finished_medicine_permission_detail.ts
+++ src/types/finished_medicine_permission_detail.ts
@@ -2,19 +2,43 @@
  * 완제 의약품 허가 상세
  */
 export interface IFinishedMedicinePermissionDetail {
-  ITEM_SEQ: string; // 품목일련번호
   ITEM_NAME: string; // 품목명
+  ITEM_ENG_NAME: string; // 품목 영문명
+  ITEM_SEQ: string; // 품목일련번호
+  APPROVAL_TYPE: string; // 허가/신고구분
+  CANCEL_STATUS: string; // 취소상태
+  CANCEL_DATE: string; // 취소일자
   ENTP_NAME: string; // 업체명
+  ENTP_ENG_NAME: string; // 업체 영문명
   ITEM_PERMIT_DATE: string; // 허가일자
+  ENTP_PERMIT_NO: string; // 업체허가번호
   ETC_OTC_CODE: string; // 전문일반
   CHART: string; // 성상
   BAR_CODE: string; // 표준코드
   MATERIAL_NAME: string; // 원료성분
-  VALID_TERM: string; // 유효기간
+  MATERIAL_ENG_NAME: string; // 영문성분명
+  EE_DOC_DATA: string; // 효능효과
+  UD_DOC_DATA: string; // 용법용량
+  NB_DOC_DATA: string; // 주의사항
+  ATT_DOC_DATA: string; // 첨부문서
   STORAGE_METHOD: string; // 저장방법
+  REEXAM_TARGET_YN: string; // 재심사대상
+  REEXAM_CONT: string; // 재심사기간
+  VALID_TERM: string; // 유효기간
   PACK_UNIT: string; // 포장단위
+  INSURANCE_CODE: string; // 보험코드
+  DRUG_CLASS: string; // 마약류분류
+  FINISH_MATERIAL_YN: string; // 완제원료구분
+  NEW_DRUG_YN: string; // 신약여부
+  INDUTY_CODE: string; // 업종구분
+  CHANGE_CONTENT: string; // 변경내용
+  TOTAL_CONTENT: string; // 총량
   MAIN_ITEM_INGR: string; // 주성분명
   INGR_NAME: string; // 첨가제명
+  ATC_CODE: string; // ATC코드
+  ENTP_BIZ_NO: string; // 사업자번호
+  RARE_DRUG_YN: string; // 희귀의약품여부
+  OEM_ENTP_NAME: string; // 위탁제조업체
 }
 
 export type TFinishedMedicinePermissionDetailResource = Record<
@@ -23,17 +47,41 @@ export type TFinishedMedicinePermissionDetailResource = Record<
 >;
 
 export const FINISHED_MEDICINE_PERMISSION_PROPERTY_MAP = {
-  품목일련번호: "ITEM_SEQ",
   품목명: "ITEM_NAME",
+  "품목 영문명": "ITEM_ENG_NAME",
+  품목일련번호: "ITEM_SEQ",
+  "허가/신고구분": "APPROVAL_TYPE",
+  취소상태: "CANCEL_STATUS",
+  취소일자: "CANCEL_DATE",
   업체명: "ENTP_NAME",
+  "업체 영문명": "ENTP_ENG_NAME",
   허가일자: "ITEM_PERMIT_DATE",
+  업체허가번호: "ENTP_PERMIT_NO",
   전문일반: "ETC_OTC_CODE",
   성상: "CHART",
   표준코드: "BAR_CODE",
   원료성분: "MATERIAL_NAME",
-  유효기간: "VALID_TERM",
+  영문성분명: "MATERIAL_ENG_NAME",
+  효능효과: "EE_DOC_DATA",
+  용법용량: "UD_DOC_DATA",
+  주의사항: "NB_DOC_DATA",
+  첨부문서: "ATT_DOC_DATA",
   저장방법: "STORAGE_METHOD",
+  유효기간: "VALID_TERM",
+  재심사대상: "REEXAM_TARGET_YN",
+  재심사기간: "REEXAM_CONT",
   포장단위: "PACK_UNIT",
+  보험코드: "INSURANCE_CODE",
+  마약류분류: "DRUG_CLASS",
+  완제원료구분: "FINISH_MATERIAL_YN",
+  신약여부: "NEW_DRUG_YN",
+  업종구분: "INDUTY_CODE",
+  변경내용: "CHANGE_CONTENT",
+  총량: "TOTAL_CONTENT",
   주성분명: "MAIN_ITEM_INGR",
   첨가제명: "INGR_NAME",
+  ATC코드: "ATC_CODE",
+  사업자번호: "ENTP_BIZ_NO",
+  희귀의약품여부: "RARE_DRUG_YN",
+  위탁제조업체: "OEM_ENTP_NAME",
 } as const;
